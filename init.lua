4e4f2a3f6f721758aa4c92481827c26e16c4a629
Konstantin Kopka <mail@ghostbutter.com>
2024-11-06 15:28:30 +0900

    Update init.lua

--- init.lua
+++ init.lua
@@ -953,14 +953,22 @@ require('lazy').setup({
   },
   {
     'stevearc/oil.nvim',
+    lazy = true,
+    keys = {
+      {
+        '-',
+        function() require('oil').toggle_float() end,
+        desc = 'Oil',
+      },
+    },
     opts = {},
     -- Optional dependencies
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    config = function()
-      vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
-      require('oil').setup()
-    end,
+    -- config = function()
+    --   vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    --   require('oil').setup()
+    -- end,
   },
   {
     'famiu/bufdelete.nvim',
